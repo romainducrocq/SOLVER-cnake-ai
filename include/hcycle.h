8db2a7b66695feb5ecc93a3b3540751a3ab3aaed
romainducrocq <romain.g.ducrocq@gmail.com>
2021-10-22 13:59:53 +0200

    refactor hamiltonian cycle

--- include/hcycle.h
+++ include/hcycle.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cassert>
 #include <cstdlib>
+
 #include <algorithm>
 #include <vector>
 #include <set>
@@ -19,19 +20,16 @@ class HamiltonianCycle{
     private:
         int m_cols, m_rows;
 
-        // prim maze generator
-        bool* m_pcells;
-        std::set<Vector2i> m_pwalls;
-        std::vector<int>* m_pmaze;
-
-        // hamiltionian cycle
         int* m_hcycle;
 
     private:
-        // prim maze generator
-        void pmaze_adj_pwalls(int pcell);
+        // test
+        void print_debug(const std::vector<int>* pmaze);
 
-        void pmaze_gen_alg();
+        // prim maze generator
+        void add_pwalls(std::set<Vector2i>& pwalls, int pcell);
+
+        void pmaze_gen_alg(std::vector<int>* pmaze);
 
         // hamiltonian cycle
         void hcycle_gen_alg();
@@ -43,16 +41,7 @@ class HamiltonianCycle{
 
         const int* get_hcycle() const;
 
-    /***
-     * TEST
-     * 
-     */
-    public:
-        void print_pmaze();
-        void print_hcycle();
-        void debug();
-     /**/
-
+        int get_next_pos(int pos);
 };
 
 #endif
--- src/hcycle.cpp
+++ src/hcycle.cpp
@@ -3,31 +3,14 @@
 HamiltonianCycle::HamiltonianCycle(int cols, int rows)
     : m_cols(cols), m_rows(rows){
 
-    assert(this->m_cols % 2 == 0);
-    assert(this->m_rows % 2 == 0);
+    assert(this->m_cols % 2 == 0 && this->m_rows % 2 == 0);
 
-    // prim maze generator
-    this->m_pcells = new bool[(this->m_cols / 2) * (this->m_rows / 2)];
-    for(int i = 0; i < (this->m_cols / 2) * (this->m_rows / 2); i++){
-        this->m_pcells[i] = false;
-    }
-
-    this->m_pmaze = new std::vector<int>[(this->m_cols / 2) * (this->m_rows / 2)];
-
-    this->pmaze_gen_alg();
-
-    // hamiltonian cycle
     this->m_hcycle = new int[this->m_cols * this->m_rows];
-
+    
     this->hcycle_gen_alg();
 }
 
 HamiltonianCycle::~HamiltonianCycle(){
-    // prim maze generator
-    delete[] this->m_pcells;
-    delete[] this->m_pmaze;
-
-    // hamiltonian cycle
     delete[] this->m_hcycle;
 }
 
@@ -36,56 +19,67 @@ HamiltonianCycle::~HamiltonianCycle(){
  * 
  */
 
-void HamiltonianCycle::pmaze_adj_pwalls(int pcell){
+void HamiltonianCycle::add_pwalls(std::set<Vector2i>& pwalls, int pcell){
     if(pcell % (this->m_cols / 2) != 0){                      //left
-        this->m_pwalls.insert(Vector2i(pcell - 1, pcell));
+        pwalls.insert(Vector2i(pcell - 1, pcell));
     }
     if((pcell + 1) % (this->m_cols / 2) != 0){                //right
-        this->m_pwalls.insert(Vector2i(pcell, pcell + 1));
+        pwalls.insert(Vector2i(pcell, pcell + 1));
     }
     if(pcell >= (this->m_cols / 2)){                          //up
-        this->m_pwalls.insert(Vector2i(pcell - (this->m_cols / 2), pcell));
+        pwalls.insert(Vector2i(pcell - (this->m_cols / 2), pcell));
     }
     if(pcell < ((this->m_rows / 2)- 1) * (this->m_cols / 2)){ //down
-        this->m_pwalls.insert(Vector2i(pcell, pcell + (this->m_cols / 2)));
+        pwalls.insert(Vector2i(pcell, pcell + (this->m_cols / 2)));
     }
 }
 
-void HamiltonianCycle::pmaze_gen_alg(){
+void HamiltonianCycle::pmaze_gen_alg(std::vector<int>* pmaze){
     // https://en.wikipedia.org/wiki/Maze_generation_algorithm
+
+    // start with a grid full of walls
+    bool* pcells;
+    std::set<Vector2i> pwalls;
+
+    pcells = new bool[(this->m_cols / 2) * (this->m_rows / 2)];
+    for(int i = 0; i < (this->m_cols / 2) * (this->m_rows / 2); i++){
+        pcells[i] = false;
+    }
 
     // pick random cell, set to visited
     int pcell = std::rand() % ((this->m_cols / 2) * (this->m_rows / 2));
-    this->m_pcells[pcell] = true;
+    pcells[pcell] = true;
 
     // add adjacent walls to wall list
-    this->pmaze_adj_pwalls(pcell);
+    this->add_pwalls(pwalls, pcell);
 
     // while wall list not empty
-    while(!this->m_pwalls.empty()){
+    while(!pwalls.empty()){
 
         // pick random wall from wall list
-        auto pwall = this->m_pwalls.begin();
-        std::advance(pwall, rand() % this->m_pwalls.size());
+        auto pwall = pwalls.begin();
+        std::advance(pwall, rand() % pwalls.size());
 
         // if wall has only one visited cell
-        if(!this->m_pcells[pwall->x] != !this->m_pcells[pwall->y]){
+        if(!pcells[pwall->x] != !pcells[pwall->y]){
             
             // wall is edge, add to maze
-            this->m_pmaze[pwall->x].push_back(pwall->y);
-            this->m_pmaze[pwall->y].push_back(pwall->x);
+            pmaze[pwall->x].push_back(pwall->y);
+            pmaze[pwall->y].push_back(pwall->x);
 
             // set unvisited cell to visited
-            pcell = this->m_pcells[pwall->x] ? pwall->y : pwall->x;
-            this->m_pcells[pcell] = true;
+            pcell = pcells[pwall->x] ? pwall->y : pwall->x;
+            pcells[pcell] = true;
 
             // add adjacent walls to wall list
-            this->pmaze_adj_pwalls(pcell);
+            this->add_pwalls(pwalls, pcell);
         }
 
         // remove wall from wall list
-        this->m_pwalls.erase(pwall);
+        pwalls.erase(pwall);
     }
+
+    delete[] pcells;
 }
 
 /***
@@ -94,6 +88,10 @@ void HamiltonianCycle::pmaze_gen_alg(){
  */
 
 void HamiltonianCycle::hcycle_gen_alg(){
+    std::vector<int>* pmaze = new std::vector<int>[(this->m_cols / 2) * (this->m_rows / 2)];
+
+    this->pmaze_gen_alg(pmaze);
+
     int hcell = 0, hdir = 0, hcount = 0;
 
     do{
@@ -107,22 +105,22 @@ void HamiltonianCycle::hcycle_gen_alg(){
             else if(i == 1){ hnext = (hdir + 4) % 4; }
             else if(i == 2){ hnext = (hdir + 5) % 4; }
 
-            if(hnext == 0 && ((hcell / this->m_cols) % 2 == 1 || v_contains(this->m_pmaze[pcell], pcell - (this->m_cols / 2)))){       // up
+            if(hnext == 0 && ((hcell / this->m_cols) % 2 == 1 || v_contains(pmaze[pcell], pcell - (this->m_cols / 2)))){       // up
                 hcell -= this->m_cols; 
                 hdir = 0;
                 break;
 
-            }else if(hnext == 1 && (hcell % 2 == 0 || v_contains(this->m_pmaze[pcell], pcell + 1))){                                   // right
+            }else if(hnext == 1 && (hcell % 2 == 0 || v_contains(pmaze[pcell], pcell + 1))){                                   // right
                 hcell += 1; 
                 hdir = 1;
                 break;
 
-            }else if(hnext == 2 && ((hcell / this->m_cols) % 2 == 0 || v_contains(this->m_pmaze[pcell], pcell + (this->m_cols / 2)))){ // down
+            }else if(hnext == 2 && ((hcell / this->m_cols) % 2 == 0 || v_contains(pmaze[pcell], pcell + (this->m_cols / 2)))){ // down
                 hcell += this->m_cols; 
                 hdir = 2;
                 break;
 
-            }else if(hnext == 3 && (hcell % 2 == 1 || v_contains(this->m_pmaze[pcell], pcell - 1))){                                   // left
+            }else if(hnext == 3 && (hcell % 2 == 1 || v_contains(pmaze[pcell], pcell - 1))){                                   // left
                 hcell -= 1; 
                 hdir = 3;
                 break;
@@ -130,10 +128,18 @@ void HamiltonianCycle::hcycle_gen_alg(){
             }
         }
     }while(hcount < this->m_cols * this->m_rows);
+
+    this->print_debug(pmaze);
+    
+    delete[] pmaze;
 }
 
 const int* HamiltonianCycle::get_hcycle() const{
     return this->m_hcycle;
+}
+
+int HamiltonianCycle::get_next_pos(int pos){
+    return this->m_hcycle[(pos + 1) % (this->m_cols * this->m_rows)];
 }
 
 /***
@@ -141,7 +147,9 @@ const int* HamiltonianCycle::get_hcycle() const{
  * 
  */
 
-void HamiltonianCycle::print_pmaze(){
+void HamiltonianCycle::print_debug(const std::vector<int>* pmaze){
+
+    // print pmaze
     std::cout << "\nMaze:\n";
     for(int i = 0; i < (this->m_cols / 2); i++){
         std::cout << " _";
@@ -150,12 +158,12 @@ void HamiltonianCycle::print_pmaze(){
     for(int i = 0; i < (this->m_rows / 2); i++){
         std::cout << "|";
         for(int j = 0; j < (this->m_cols / 2); j++){
-            if(v_contains(this->m_pmaze[i * (this->m_cols / 2) + j], (i + 1) * (this->m_cols / 2) + j)){
+            if(v_contains(pmaze[i * (this->m_cols / 2) + j], (i + 1) * (this->m_cols / 2) + j)){
                 std::cout << " "; 
             }else{
                 std::cout << "_"; 
             }
-            if(v_contains(this->m_pmaze[i * (this->m_cols / 2) + j], i * (this->m_cols / 2) + (j + 1))){
+            if(v_contains(pmaze[i * (this->m_cols / 2) + j], i * (this->m_cols / 2) + (j + 1))){
                 std::cout << " "; 
             }else{
                 std::cout << "|"; 
@@ -163,17 +171,11 @@ void HamiltonianCycle::print_pmaze(){
         }
         std::cout << "\n";
     }
-}
 
-void HamiltonianCycle::print_hcycle(){
+    // print hcycle
     std::cout << "\nHamiltonian cycle:\n\n"; 
     for(int i = 0; i < this->m_cols * this->m_rows; i++){
         std::cout << this->m_hcycle[i] << " ";
     }
     std::cout << "\n";
-}
-
-void HamiltonianCycle::debug(){
-    this->print_pmaze();
-    this->print_hcycle();
 }
